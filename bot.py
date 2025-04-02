7993eed865edf005909f2e9aeb0d3873261d59f3
Slithon <markianvolanik52@gmail.com>
2025-04-02 16:37:31 +0300

    1

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ def check_and_update_version():
         exit(1)
 
 check_and_update_version()
-
+print("Бот запущено успішно")
 # ==================== Допоміжна функція для роботи з базою даних ====================
 def execute_db(query, params=None, fetchone=False, commit=False):
     try:
@@ -1018,4 +1018,5 @@ def all_text(message):
     send_commands_menu(message)
 
 # ==================== Запуск бота ====================
+print("Бот запущено успішно")
 bot.polling()
